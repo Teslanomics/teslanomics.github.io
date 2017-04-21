8b765deaa11f6fa2836a651f06c4100b65b16616
HaoZeke <driger633@gmail.com>
2017-04-21 15:03:57 +0530

    Test plugin CI

--- src/_plugins/jekyll-feed.rb
+++ src/_plugins/jekyll-feed.rb
@@ -3,8 +3,8 @@ require "fileutils"
 require_relative "./jekyll-feed/generator"
 
 module JekyllFeed
-  autoload :MetaTag,          "./jekyll-feed/meta-tag"
-  autoload :PageWithoutAFile, "./jekyll-feed/page-without-a-file.rb"
+  autoload :MetaTag,          "jekyll-feed/meta-tag"
+  autoload :PageWithoutAFile, "jekyll-feed/page-without-a-file.rb"
 end
 
 Liquid::Template.register_tag "feed_meta", JekyllFeed::MetaTag
