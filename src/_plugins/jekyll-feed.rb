07b6afef378e36f001bd7b5694c7bf98bc01031d
HaoZeke <driger633@gmail.com>
2017-04-21 15:21:03 +0530

    Ugly plugin hack for CI

--- src/_plugins/jekyll-feed.rb
+++ src/_plugins/jekyll-feed.rb
@@ -3,8 +3,8 @@ require "fileutils"
 require_relative "./jekyll-feed/generator"
 
 module JekyllFeed
-  autoload :MetaTag,          "jekyll-feed/meta-tag"
-  autoload :PageWithoutAFile, "jekyll-feed/page-without-a-file.rb"
+  require_relative          "jekyll-feed/meta-tag"
+  require_relative 			"jekyll-feed/page-without-a-file.rb"
 end
 
 Liquid::Template.register_tag "feed_meta", JekyllFeed::MetaTag
